430290a2a0500901a7039bfb2679f2940880663c
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-03-24 13:30:33 -0600

    resize

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -7,6 +7,25 @@ const material = new THREE.MeshBasicMaterial({color: "red"})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+//Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 
 scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+console.log(canvas)
+const renderer = new THREE.WbGLRenderer({
+    canvas
+})
+
+// resize renderer
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
