f5058f7445121d943f4b557caf00639d0a57e588
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-03-24 16:57:20 -0600

    chapter 3

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,31 +1,31 @@
-// Scene
-const scene = new THREE.Scene()
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
-// Red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: "red"})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-//Sizes
+// Sizes
 const sizes = {
     width: 800,
     height: 600
 }
 
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: '#ff0000'
+})
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+scene.add(cubeMesh)
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 
+camera.position.z = 3
 scene.add(camera)
 
 // Renderer
-const canvas = document.querySelector('.webgl')
-console.log(canvas)
-const renderer = new THREE.WbGLRenderer({
-    canvas
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
 })
-
-// resize renderer
 renderer.setSize(sizes.width, sizes.height)
-
 renderer.render(scene, camera)
