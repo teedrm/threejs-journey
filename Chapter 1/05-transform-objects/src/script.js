78f1d4ca80ed07bb5c9e4e4e1e7afe1943df83d1
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-03-24 20:31:53 -0600

    transform, translate,scale

--- Chapter 1/05-transform-objects/src/script.js
+++ Chapter 1/05-transform-objects/src/script.js
@@ -9,22 +9,55 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-mesh.position.x = 0.7
-mesh.position.y = -0.6
-mesh.position.z = 1 
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1 
 
-// mesh.position.set(0.7, -0.6, 1) => x,y,z
+// // mesh.position.set(0.7, -0.6, 1) => x,y,z
 
-scene.add(mesh)
+// scene.add(mesh)
+
+// //Scale
+// mesh.scale.set(2, 0.5, 0.5)
+
+// // Rotation
+// mesh.rotation.reorder('YXZ')
+// // half rotation = Math.PI
+// mesh.rotation.y = Math.PI/2
 
 // console.log('Vector length = distance between center of scene to object', mesh.position.length())
 
 // normalize will reduce the length of vector to 1
 // mesh.position.normalize()
 // console.log(mesh.position.length())
+
+
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({ color: 0xff0000})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({ color: 0xff000})
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff})
+)
+cube3.position.x = 2
+group.add(cube3)
+
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper()
@@ -44,6 +77,10 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+// camera is looking straight at object
+// camera.lookAt(mesh.position)
+
 
 // console.log('distance from camera', mesh.position.distanceTo(camera.position))
 
