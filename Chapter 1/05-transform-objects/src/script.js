4babd088e03cdad410a127dff32ae40dc9f8fe73
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-03-24 19:56:29 -0600

    organize folders

--- Chapter 1/05-transform-objects/src/script.js
+++ Chapter 1/05-transform-objects/src/script.js
@@ -12,7 +12,23 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.x = 0.7
+mesh.position.y = -0.6
+mesh.position.z = 1 
+
+// mesh.position.set(0.7, -0.6, 1) => x,y,z
+
 scene.add(mesh)
+
+// console.log('Vector length = distance between center of scene to object', mesh.position.length())
+
+// normalize will reduce the length of vector to 1
+// mesh.position.normalize()
+// console.log(mesh.position.length())
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -28,6 +44,8 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+// console.log('distance from camera', mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer
